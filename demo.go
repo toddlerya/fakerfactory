d61b06f85e5d43bbe858736b8f0b57dc5fadbd71
todderya <5418150+toddlerya@users.noreply.github.com>
2018-02-12 10:42:58 +0800

    Name() ok

--- demo.go
+++ demo.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 
 	"github.com/toddlerya/FakerHub/faker"
-//	"github.com/toddlerya/FakerHub/server"
+	//	"github.com/toddlerya/FakerHub/server"
 )
 
 func main() {
-	faker.Seed(11)
-	fmt.Println(faker.Color("zh_CN"))
-	fmt.Println(faker.Color("en_US", "zh_CN"))
+	faker.Seed(0)
+	//fmt.Println(faker.Color("zh_CN"))
+	//fmt.Println(faker.Color("en_US", "zh_CN"))
+	fmt.Println(faker.Name("en_US", "zh_CN"))
 	//fmt.Println(faker.UserAgent())
 	//server.StartServer()
 }
---
+++ faker/name.go
@@ -0,0 +1,43 @@
+package faker
+
+import (
+	"math/rand"
+	"strings"
+)
+
+// Name will generate a random First and Last Name
+func Name(langs ...string) string {
+	// lang: zh_CN --> 中文
+	// lang: en_US --> 英文
+	lang := langs[rand.Intn(len(langs))]
+	first := strings.Join([]string{lang, "first"}, "_")
+	last := strings.Join([]string{lang, "last"}, "_")
+	switch lang {
+	case "en_US":
+		return getRandValue([]string{"person", first}) + " " + getRandValue([]string{"person", last})
+	case "zh_CN":
+		return getRandValue([]string{"person", last}) + getRandValue([]string{"person", first})
+	default:
+		return "未知的语言类型[姓名报错]"
+	}
+}
+
+// FirstName will generate a random first name
+func FirstName() string {
+	return getRandValue([]string{"person", "first"})
+}
+
+// LastName will generate a random last name
+func LastName() string {
+	return getRandValue([]string{"person", "last"})
+}
+
+// NamePrefix will generate a random name prefix
+func NamePrefix() string {
+	return getRandValue([]string{"person", "prefix"})
+}
+
+// NameSuffix will generate a random name suffix
+func NameSuffix() string {
+	return getRandValue([]string{"person", "suffix"})
+}
