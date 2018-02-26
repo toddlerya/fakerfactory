0b3fedfa7a21e4a074ff33813c44ad97928e1f49
toddlerya <5418150+toddlerya@users.noreply.github.com>
2018-02-26 21:20:07 +0800

    add trans mysql to sqlite

--- tool/cnarea.go
+++ tool/cnarea.go
@@ -5,93 +5,117 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func queryMariaDB() {
-	db, err := sql.Open("mysql", "user:passwd@tcp(192.168.1.108:3306)/fakerhub?charset=utf8")
-	if err != nil {
-		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
-	}
-	defer db.Close()
+var dbPath string = "../faker/data/cnarea.db"
 
-	// Execute the query
-	//	querySql := "SELECT * FROM cnarea_2016 LIMIT 10"
-	querySql := "SELECT count(1) FROM cnarea_2016"
-	rows, err := db.Query(querySql)
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
+func initSqlite() {
+	// 连接数据库
+	os.Remove(dbPath)
 
-	// Get column names
-	columns, err := rows.Columns()
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
-
-	// Make a slice for the values
-	values := make([]sql.RawBytes, len(columns))
-
-	// rows.Scan wants '[]interface{}' as an argument, so we must copy the
-	// references into such a slice
-	// See http://code.google.com/p/go-wiki/wiki/InterfaceSlice for details
-	scanArgs := make([]interface{}, len(values))
-	for i := range values {
-		scanArgs[i] = &values[i]
-	}
-
-	// Fetch rows
-	for rows.Next() {
-		// get RawBytes from data
-		err = rows.Scan(scanArgs...)
-		if err != nil {
-			panic(err.Error()) // proper error handling instead of panic in your app
-		}
-
-		// Now do something with the data.
-		// Here we just print each column as a string.
-		var value string
-		for i, col := range values {
-			// Here we can check if the value is nil (NULL value)
-			if col == nil {
-				value = "NULL"
-			} else {
-				value = string(col)
-			}
-			fmt.Println(columns[i], ": ", value)
-		}
-		fmt.Println("-----------------------------------")
-	}
-	if err = rows.Err(); err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
-}
-
-func insertSqlite() {
-	dbFilePath := "../faker/data/cnarea.db"
-
-	os.Remove(dbFilePath)
-
-	db, err := sql.Open("sqlite3", dbFilePath)
+	db, err := sql.Open("sqlite3", dbPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	sqlStmt := `
-	create table foo (id integer not null primary key, name text);
-	delete from foo;
-	`
-	_, err = db.Exec(sqlStmt)
+	// 建表
+	createSql := `CREATE TABLE IF NOT EXISTS cnarea_2016 (
+		uid INTEGER PRIMARY KEY AUTOINCREMENT,
+		area_code VARCHAR(64),
+		zip_code VARCHAR(64),
+		city_code VARCHAR(64),
+		areaname VARCHAR(64),
+		name VARCHAR(64),
+		short_name VARCHAR(64),
+		lng VARCHAR(64),
+		lat VARCHAR(64)
+	)`
+
+	_, err = db.Exec(createSql)
 	if err != nil {
-		log.Printf("%q: %s\n", err, sqlStmt)
+		log.Panicf("%q: %s\n", err, createSql)
 		return
 	}
 }
 
+func exetractMySQL() ([]map[string]string, int) {
+	db, err := sql.Open("mysql", "evi1:w5nbb@tcp(192.168.1.108:3306)/fakerhub?charset=utf8")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// 统计条数
+	countSql := "SELECT count(1) num FROM cnarea_2016 WHERE level = 4"
+	var num string
+	row := db.QueryRow(countSql)
+	err = row.Scan(&num)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dataNumber, err := strconv.Atoi(num)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("共计%d条数据\n", dataNumber)
+
+	var data []map[string]string
+
+	// 查询
+	querySql := "SELECT area_code, zip_code, city_code, name, short_name, merger_name, lng, lat FROM cnarea_2016 WHERE level = 4 LIMIT 10"
+	rows, err := db.Query(querySql)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for rows.Next() {
+		var area_code, zip_code, city_code, name, short_name, merger_name, lng, lat string
+		if err := rows.Scan(&area_code, &zip_code, &city_code, &name, &short_name, &merger_name, &lng, &lat); err != nil {
+			log.Fatal(err)
+		}
+		tempMergerName := strings.Split(merger_name, ",")
+		if len(tempMergerName) == 0 {
+			tempMergerName[0] = "地市地址拆分错误"
+		} else {
+			tempMergerName = tempMergerName[0 : len(tempMergerName)-1]
+		}
+		areaname := strings.Join(tempMergerName, ",")
+		//		fmt.Println(area_code, zip_code, city_code, areaname, name, short_name, lng, lat)
+		row := make(map[string]string)
+		row["area_code"] = area_code
+		row["zip_code"] = zip_code
+		row["city_code"] = city_code
+		row["areaname"] = areaname
+		row["name"] = name
+		row["short_name"] = short_name
+		row["lng"] = lng
+		row["lat"] = lat
+		data = append(data, row)
+	}
+	return data, dataNumber
+}
+
+func loadSqlite(data []map[string]string) {
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	for col, num := range data {
+		fmt.Println(col, num)
+	}
+
+}
+
 func main() {
-	//	queryMariaDB()
-	insertSqlite()
+	initSqlite()
+	mysqlData, dataNum := exetractMySQL()
+	fmt.Printf("total columns: %d", dataNum)
+	loadSqlite(mysqlData)
 }
